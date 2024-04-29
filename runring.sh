a537a63c9bd00029041eaee83a98760a0d62e849
finrxpro <162915123+finrxpro@users.noreply.github.com>
2024-04-29 15:06:29 +0300

    Update runring.sh

--- runring.sh
+++ runring.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "Path to root:"
 pwd
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:ring/lib
-ring/bin/ring -cgi $1 $2 $3 
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/render/project/src/ring/lib
+/opt/render/project/src/ring/bin/ring -cgi $1 $2 $3 
